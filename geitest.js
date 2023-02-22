a3a546d9d802bfaa900214cc036ac6a3ccb28ad5
Sebastián Verdugo <125592886+SebaVerdugod@users.noreply.github.com>
2023-02-22 13:39:29 -0300

    Add files via upload

---
+++ geitest.js
@@ -0,0 +1,22 @@
+
+console.log("a");
+const btnci = document.getElementById("ci");
+const btnnon = document.getElementById("non");
+
+const formulario = document.getElementById('quest');
+const enviarButton = document.getElementById('ok_jusgame');
+enviarButton.addEventListener('click', function() {
+  const ci = document.querySelector('input[id="ci"]:checked');
+  const non = document.querySelector('input[id="non"]:checked');
+  if (ci || non) {
+    let respuesta = ci ? 'sí' : 'no';
+    if (ci) {
+      alert('Weko');
+    } else {
+      alert('q grande');
+    }
+  } else {
+    alert('responde la wea');
+  }
+});
+
